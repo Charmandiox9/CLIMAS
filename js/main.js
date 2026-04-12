e441a35b93d9e7d34f971c91d412011b3ad0ae5a
Daniel Durán <daninsoi924@gmail.com>
2026-04-12 19:21:55 -0400

    feat: Implementación de página Agendar Cita con datos mockeados

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 import { formatDate } from './modules/utils.js';
 import { Navbar } from './components/navbar.js'
+import { initScheduleEvents, SchedulePage } from './pages/Schedule.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('navbar').innerHTML = Navbar()
+  
+  const scheduleContainer = document.getElementById('schedule-page');
+
+  if(scheduleContainer) {
+    scheduleContainer.innerHTML = SchedulePage();
+  }
+  initScheduleEvents();
 });
 
