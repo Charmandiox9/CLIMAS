d56fb70f7fa6759279c303248abea72b9346d65c
Diego Contreras <diegocontreraskwk14@gmail.com>
2026-06-15 12:16:39 -0400

    DELETE

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -109,8 +109,22 @@ export class UserService {
     });
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user) throw new BadRequestException('Usuario no encontrado');
+
+    return this.prisma.user.delete({
+      where: { id },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        rut: true,
+        email: true,
+        roles: true,
+        isActive: true,
+      },
+    });
   }
 
   async createDoctor(createDoctorDto: CreateDoctorDto) {
