049e9ff8cda6b32073a1b831ac48e26a3bc1c8e3
Daniel Durán <daninsoi924@gmail.com>
2026-06-20 17:07:22 -0400

    feat: add flutter_local_notifications plugin and enable core library desugaring for Android

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import file_selector_macos
 import firebase_core
 import firebase_messaging
 import firebase_storage
+import flutter_local_notifications
 import gal
 import geolocator_apple
 import local_auth_darwin
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
